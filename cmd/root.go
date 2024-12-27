b3659f44c4819207b79cff311969e6077127245f
Anthony Gonzalez <anthony.gonzalez@wattpad.com>
2024-12-27 18:11:32 +1100

    Basic command app

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:     "merenpic",
+	Version: "0.0.1",
+	Short:   "Merenpic CLI tool",
+	Long:    `Merenpic is a CLI tool that help you organize your photos and videos from a Google Photos takeout.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2024 Anthony Gonzalez info@merensoft.ca
+*/
+package main
+
+import "github.com/merensoft/merenpic/cmd"
+
+func main() {
+	cmd.Execute()
+}
